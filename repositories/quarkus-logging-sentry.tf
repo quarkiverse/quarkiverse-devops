5de1d42f02cd4e5619796304611ac718d9146aae
George Gastaldi <gegastaldi@gmail.com>
2021-10-28 12:33:52 -0300

    Add sentry extension

---
+++ repositories/quarkus-logging-sentry.tf
@@ -0,0 +1,29 @@
+# Create repository
+resource "github_repository" "quarkus_logging_sentry" {
+  name = "quarkus-logging-sentry"
+  description = "Quarkus extension for Sentry, a self-hosted or cloud-based error monitoring solution"
+  delete_branch_on_merge = true
+  topics = ["quarkus-extension"]
+}
+
+# Create team
+resource "github_team" "quarkus_logging_sentry" {
+  name                      = "quarkiverse-logging-sentry"
+  description               = "Quarkiverse team for the Sentry Logging extension"
+  create_default_maintainer = true
+}
+
+# Add team to repository
+resource "github_team_repository" "quarkus_logging_sentry" {
+  team_id    = github_team.quarkus_logging_sentry.id
+  repository = github_repository.quarkus_logging_sentry.name
+  permission = "maintain"
+}
+
+# Add users to the team
+resource "github_team_membership" "quarkus_logging_sentry" {
+  for_each = { for tm in ["gsmet"] : tm => tm }
+  team_id  = github_team.quarkus_logging_sentry.id
+  username = each.value
+  role = "maintainer"
+}
