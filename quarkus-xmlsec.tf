614980963f48f5155105e9daa6dc494a98291382
George Gastaldi <gegastaldi@gmail.com>
2021-10-28 23:24:36 -0300

    Included quarkus-xmlsec.tf

---
+++ quarkus-xmlsec.tf
@@ -0,0 +1,30 @@
+# Create repository
+resource "github_repository" "quarkus_xmlsec" {
+  name                   = "quarkus-xmlsec"
+  description            = "Quarkus XML Security extension"
+  delete_branch_on_merge = true
+  topics                 = ["quarkus-extension"]
+}
+
+# Create team
+resource "github_team" "quarkus_xmlsec" {
+  name                      = "quarkiverse-xmlsec"
+  description               = "Quarkiverse team for the XML Security extension"
+  create_default_maintainer = false
+  privacy                   = "closed"
+}
+
+# Add team to repository
+resource "github_team_repository" "quarkus_xmlsec" {
+  team_id    = github_team.quarkus_xmlsec.id
+  repository = github_repository.quarkus_xmlsec.name
+  permission = "maintain"
+}
+
+# Add users to the team
+resource "github_team_membership" "quarkus_xmlsec" {
+  for_each = { for tm in ["gastaldi"] : tm => tm }
+  team_id  = github_team.quarkus_xmlsec.id
+  username = each.value
+  role     = "maintainer"
+}
