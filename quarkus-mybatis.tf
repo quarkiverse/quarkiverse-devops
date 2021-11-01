f840012ae91c8b1dcd79e79d91ba5915c86909cd
George Gastaldi <gegastaldi@gmail.com>
2021-11-01 12:09:55 -0300

    Add quarkus-mybatis

---
+++ quarkus-mybatis.tf
@@ -0,0 +1,41 @@
+# Create repository
+resource "github_repository" "quarkus_mybatis" {
+  name                   = "quarkus-mybatis"
+  description            = "Quarkus MyBatis Extension"
+  delete_branch_on_merge = true
+  has_issues             = true
+  has_projects           = true
+  #has_discussions        = true
+  vulnerability_alerts = true
+
+  topics = ["quarkus-extension"]
+
+  # Do not use the template below in new repositories. This is kept for backward compatibility with existing repositories
+  template {
+    owner      = "quarkiverse"
+    repository = "quarkiverse-template"
+  }
+}
+
+# Create team
+resource "github_team" "quarkus_mybatis" {
+  name                      = "quarkiverse-mybatis"
+  description               = "Quarkiverse team for the mybatis extension"
+  create_default_maintainer = false
+  privacy                   = "closed"
+}
+
+# Add team to repository
+resource "github_team_repository" "quarkus_mybatis" {
+  team_id    = github_team.quarkus_mybatis.id
+  repository = github_repository.quarkus_mybatis.name
+  permission = "maintain"
+}
+
+# Add users to the team
+resource "github_team_membership" "quarkus_mybatis" {
+  for_each = { for tm in ["zhfeng", "zohar-soul"] : tm => tm }
+  team_id  = github_team.quarkus_mybatis.id
+  username = each.value
+  role     = "maintainer"
+}
