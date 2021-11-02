96be5aaceb4af67e593d036b585b29abcc078cd0
George Gastaldi <gegastaldi@gmail.com>
2021-11-02 20:49:31 -0300

    Add quarkus-reactive-messaging-http.tf

---
+++ quarkus-reactive-messaging-http.tf
@@ -0,0 +1,32 @@
+# Create repository
+resource "github_repository" "quarkus_reactive_messaging_http" {
+  name                   = "quarkus-reactive-messaging-http"
+  description            = "Connect to HTTP or Web Socket and expose HTTP or Web Socket endpoints for Reactive Messaging"
+  delete_branch_on_merge = true
+  has_issues             = true
+  vulnerability_alerts   = true
+  topics                 = ["quarkus-extension"]
+}
+
+# Create team
+resource "github_team" "quarkus_reactive_messaging_http" {
+  name                      = "quarkiverse-reactive-messaging-http"
+  description               = "Quarkiverse team for the Reactive Messaging HTTP extension"
+  create_default_maintainer = false
+  privacy                   = "closed"
+}
+
+# Add team to repository
+resource "github_team_repository" "quarkus_reactive_messaging_http" {
+  team_id    = github_team.quarkus_reactive_messaging_http.id
+  repository = github_repository.quarkus_reactive_messaging_http.name
+  permission = "maintain"
+}
+
+# Add users to the team
+resource "github_team_membership" "quarkus_reactive_messaging_http" {
+  for_each = { for tm in ["michalszynkiewicz"] : tm => tm }
+  team_id  = github_team.quarkus_reactive_messaging_http.id
+  username = each.value
+  role     = "maintainer"
+}
