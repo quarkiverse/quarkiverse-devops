49fc49922d87f6599d9076fba57de4de767e3fda
George Gastaldi <gegastaldi@gmail.com>
2021-11-03 10:16:55 -0300

    Add quarkus-rsocket.tf

---
+++ quarkus-rsocket.tf
@@ -0,0 +1,32 @@
+# Create repository
+resource "github_repository" "quarkus_rsocket" {
+  name                   = "quarkus-rsocket"
+  description            = "Quarkus RSocket extension"
+  delete_branch_on_merge = true
+  has_issues             = true
+  vulnerability_alerts   = true
+  topics                 = ["quarkus-extension"]
+}
+
+# Create team
+resource "github_team" "quarkus_rsocket" {
+  name                      = "quarkiverse-rsocket"
+  description               = "Quarkiverse team for the rsocket extension"
+  create_default_maintainer = false
+  privacy                   = "closed"
+}
+
+# Add team to repository
+resource "github_team_repository" "quarkus_rsocket" {
+  team_id    = github_team.quarkus_rsocket.id
+  repository = github_repository.quarkus_rsocket.name
+  permission = "maintain"
+}
+
+# Add users to the team
+resource "github_team_membership" "quarkus_rsocket" {
+  for_each = { for tm in ["dufoli"] : tm => tm }
+  team_id  = github_team.quarkus_rsocket.id
+  username = each.value
+  role     = "maintainer"
+}
