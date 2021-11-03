f1bfbd0753a29568daa3cc9d8502e5a8ba6c8a2b
George Gastaldi <gegastaldi@gmail.com>
2021-11-02 21:35:42 -0300

    Add quarkus-apicurio-registry-client.tf

---
+++ quarkus-apicurio-registry-client.tf
@@ -0,0 +1,38 @@
+# Create repository
+resource "github_repository" "quarkus_apicurio_registry_client" {
+  name                   = "quarkus-apicurio-registry-client"
+  description            = "Quarkus Extension for Apicurio Registry Client"
+  delete_branch_on_merge = true
+  has_issues             = true
+  vulnerability_alerts   = true
+  topics                 = ["quarkus-extension"]
+
+  # Do not use the template below in new repositories. This is kept for backward compatibility with existing repositories
+  template {
+    owner      = "quarkiverse"
+    repository = "quarkiverse-template"
+  }
+}
+
+# Create team
+resource "github_team" "quarkus_apicurio_registry_client" {
+  name                      = "quarkiverse-apicurio-registry-client"
+  description               = "Quarkiverse team for the apicurio-registry-client extension"
+  create_default_maintainer = false
+  privacy                   = "closed"
+}
+
+# Add team to repository
+resource "github_team_repository" "quarkus_apicurio_registry_client" {
+  team_id    = github_team.quarkus_apicurio_registry_client.id
+  repository = github_repository.quarkus_apicurio_registry_client.name
+  permission = "maintain"
+}
+
+# Add users to the team
+resource "github_team_membership" "quarkus_apicurio_registry_client" {
+  for_each = { for tm in ["famartinrh"] : tm => tm }
+  team_id  = github_team.quarkus_apicurio_registry_client.id
+  username = each.value
+  role     = "maintainer"
+}
