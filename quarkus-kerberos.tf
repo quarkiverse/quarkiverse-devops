383b16204ac2fc1e4c5104cb8a20a6ae7def662d
George Gastaldi <gegastaldi@gmail.com>
2021-11-03 14:10:01 -0300

    Add quarkus-kerberos.tf

---
+++ quarkus-kerberos.tf
@@ -0,0 +1,38 @@
+# Create repository
+resource "github_repository" "quarkus_kerberos" {
+  name                   = "quarkus-kerberos"
+  description            = "Quarkus Kerberos extension"
+  delete_branch_on_merge = true
+  has_issues             = true
+  vulnerability_alerts   = true
+  topics                 = ["authentication", "quarkus-extension"]
+
+  # Do not use the template below in new repositories. This is kept for backward compatibility with existing repositories
+  template {
+    owner      = "quarkiverse"
+    repository = "quarkiverse-template"
+  }
+}
+
+# Create team
+resource "github_team" "quarkus_kerberos" {
+  name                      = "quarkiverse-kerberos"
+  description               = "Quarkiverse team for the kerberos extension"
+  create_default_maintainer = false
+  privacy                   = "closed"
+}
+
+# Add team to repository
+resource "github_team_repository" "quarkus_kerberos" {
+  team_id    = github_team.quarkus_kerberos.id
+  repository = github_repository.quarkus_kerberos.name
+  permission = "maintain"
+}
+
+# Add users to the team
+resource "github_team_membership" "quarkus_kerberos" {
+  for_each = { for tm in ["cescoffier", "sberyozkin", "stuartwdouglas"] : tm => tm }
+  team_id  = github_team.quarkus_kerberos.id
+  username = each.value
+  role     = "maintainer"
+}
