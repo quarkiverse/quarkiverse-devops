e19585ca91ce7672c2c179059efa7a3e3974169e
George Gastaldi <gegastaldi@gmail.com>
2021-11-02 21:17:45 -0300

    Add quarkus-github-app.tf

---
+++ quarkus-github-app.tf
@@ -0,0 +1,42 @@
+# Create repository
+resource "github_repository" "quarkus_github_app" {
+  name                   = "quarkus-github-app"
+  description            = "Develop your GitHub Apps in Java with Quarkus."
+  delete_branch_on_merge = true
+  has_issues             = true
+  has_projects           = true
+  vulnerability_alerts   = true
+  homepage_url           = "https://quarkiverse.github.io/quarkiverse-docs/quarkus-github-app/dev/index.html"
+  topics                 = ["quarkus-extension"]
+}
+
+# Create team
+resource "github_team" "quarkus_github_app" {
+  name                      = "quarkiverse-github-app"
+  description               = "Quarkiverse team for the GitHub App extension"
+  create_default_maintainer = false
+  privacy                   = "closed"
+}
+
+# Add team to repository
+resource "github_team_repository" "quarkus_github_app" {
+  team_id    = github_team.quarkus_github_app.id
+  repository = github_repository.quarkus_github_app.name
+  permission = "maintain"
+}
+
+# Add users to the team
+resource "github_team_membership" "quarkus_github_app" {
+  for_each = { for tm in ["gsmet", "quarkiversebot"] : tm => tm }
+  team_id  = github_team.quarkus_github_app.id
+  username = each.value
+  role     = "maintainer"
+}
+
+# Add outside collaborators to the repository
+resource "github_repository_collaborator" "quarkus_github_app" {
+  for_each   = { for tm in ["yrodiere"] : tm => tm }
+  repository = github_repository.quarkus_github_app.name
+  username   = each.value
+  permission = "push"
+}
