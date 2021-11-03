e7b021ac49e7c1d64c8c10222bea96a7573f5aad
George Gastaldi <gegastaldi@gmail.com>
2021-11-02 21:29:48 -0300

    Add quarkus-qson.tf

---
+++ quarkus-qson.tf
@@ -0,0 +1,32 @@
+# Create repository
+resource "github_repository" "quarkus_qson" {
+  name                   = "quarkus-qson"
+  description            = "Quarkus Extension for the QSon JSON Parser"
+  delete_branch_on_merge = true
+  has_issues             = true
+  vulnerability_alerts   = true
+  topics                 = ["quarkus-extension"]
+}
+
+# Create team
+resource "github_team" "quarkus_qson" {
+  name                      = "quarkiverse-qson"
+  description               = "Quarkiverse team for the Qson extension"
+  create_default_maintainer = false
+  privacy                   = "closed"
+}
+
+# Add team to repository
+resource "github_team_repository" "quarkus_qson" {
+  team_id    = github_team.quarkus_qson.id
+  repository = github_repository.quarkus_qson.name
+  permission = "maintain"
+}
+
+# Add users to the team
+resource "github_team_membership" "quarkus_qson" {
+  for_each = { for tm in ["patriot1burke"] : tm => tm }
+  team_id  = github_team.quarkus_qson.id
+  username = each.value
+  role     = "maintainer"
+}
