18a3eaf55c1fafc89ba43d03816c97a734d4848f
George Gastaldi <gegastaldi@gmail.com>
2021-11-03 09:46:51 -0300

    Add quarkus-jooq.tf

---
+++ quarkus-jooq.tf
@@ -0,0 +1,38 @@
+# Create repository
+resource "github_repository" "quarkus_jooq" {
+  name                   = "quarkus-jooq"
+  description            = "Quarkus Jooq Extension"
+  delete_branch_on_merge = true
+  has_issues             = true
+  vulnerability_alerts   = true
+  topics                 = ["quarkus-extension"]
+
+  # Do not use the template below in new repositories. This is kept for backward compatibility with existing repositories
+  template {
+    owner      = "quarkiverse"
+    repository = "quarkiverse-template"
+  }
+}
+
+# Create team
+resource "github_team" "quarkus_jooq" {
+  name                      = "quarkiverse-jooq"
+  description               = "Quarkiverse team for the jooq extension"
+  create_default_maintainer = false
+  privacy                   = "closed"
+}
+
+# Add team to repository
+resource "github_team_repository" "quarkus_jooq" {
+  team_id    = github_team.quarkus_jooq.id
+  repository = github_repository.quarkus_jooq.name
+  permission = "maintain"
+}
+
+# Add users to the team
+resource "github_team_membership" "quarkus_jooq" {
+  for_each = { for tm in ["angrymango"] : tm => tm }
+  team_id  = github_team.quarkus_jooq.id
+  username = each.value
+  role     = "maintainer"
+}
