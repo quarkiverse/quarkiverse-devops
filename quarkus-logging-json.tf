ed79479e701715d4345d9d22718bb81d5f7a1b65
George Gastaldi <gegastaldi@gmail.com>
2021-11-03 09:25:11 -0300

    Add quarkus-logging-json.tf

---
+++ quarkus-logging-json.tf
@@ -0,0 +1,32 @@
+# Create repository
+resource "github_repository" "quarkus_logging_json" {
+  name                   = "quarkus-logging-json"
+  description            = "Quarkus logging extension outputting the logging in json."
+  delete_branch_on_merge = true
+  has_issues             = true
+  vulnerability_alerts   = true
+  topics                 = ["json", "logging", "hacktoberfest", "quarkus", "quarkus-extension"]
+}
+
+# Create team
+resource "github_team" "quarkus_logging_json" {
+  name                      = "quarkiverse-logging-json"
+  description               = "Quarkiverse team for the logging-json extension"
+  create_default_maintainer = false
+  privacy                   = "closed"
+}
+
+# Add team to repository
+resource "github_team_repository" "quarkus_logging_json" {
+  team_id    = github_team.quarkus_logging_json.id
+  repository = github_repository.quarkus_logging_json.name
+  permission = "maintain"
+}
+
+# Add users to the team
+resource "github_team_membership" "quarkus_logging_json" {
+  for_each = { for tm in ["SlyngDK"] : tm => tm }
+  team_id  = github_team.quarkus_logging_json.id
+  username = each.value
+  role     = "maintainer"
+}
