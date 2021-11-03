9718bb76d64b4820ddd0dc54a6306e32267b3414
George Gastaldi <gegastaldi@gmail.com>
2021-11-03 12:54:09 -0300

    Add quarkus-jna.tf

---
+++ quarkus-jna.tf
@@ -0,0 +1,38 @@
+# Create repository
+resource "github_repository" "quarkus_jna" {
+  name                   = "quarkus-jna"
+  description            = "Java Native Access (JNA) Quarkus Extension - https://github.com/java-native-access/jna"
+  delete_branch_on_merge = true
+  has_issues             = true
+  vulnerability_alerts   = true
+  topics                 = ["quarkus-extension"]
+
+  # Do not use the template below in new repositories. This is kept for backward compatibility with existing repositories
+  template {
+    owner      = "quarkiverse"
+    repository = "quarkiverse-template"
+  }
+}
+
+# Create team
+resource "github_team" "quarkus_jna" {
+  name                      = "quarkiverse-jna"
+  description               = "Quarkiverse team for the jna extension"
+  create_default_maintainer = false
+  privacy                   = "closed"
+}
+
+# Add team to repository
+resource "github_team_repository" "quarkus_jna" {
+  team_id    = github_team.quarkus_jna.id
+  repository = github_repository.quarkus_jna.name
+  permission = "maintain"
+}
+
+# Add users to the team
+resource "github_team_membership" "quarkus_jna" {
+  for_each = { for tm in ["dufoli"] : tm => tm }
+  team_id  = github_team.quarkus_jna.id
+  username = each.value
+  role     = "maintainer"
+}
