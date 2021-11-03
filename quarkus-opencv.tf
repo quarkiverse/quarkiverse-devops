7a3a3afb2a69ed3abceac7be2d660e8bfe02bcfa
George Gastaldi <gegastaldi@gmail.com>
2021-11-03 13:50:44 -0300

    Add quarkus-opencv.tf

---
+++ quarkus-opencv.tf
@@ -0,0 +1,38 @@
+# Create repository
+resource "github_repository" "quarkus_opencv" {
+  name                   = "quarkus-opencv"
+  description            = "opencv Quarkus Extension"
+  delete_branch_on_merge = true
+  has_issues             = true
+  vulnerability_alerts   = true
+  topics                 = ["quarkus-extension"]
+
+  # Do not use the template below in new repositories. This is kept for backward compatibility with existing repositories
+  template {
+    owner      = "quarkiverse"
+    repository = "quarkiverse-template"
+  }
+}
+
+# Create team
+resource "github_team" "quarkus_opencv" {
+  name                      = "quarkiverse-opencv"
+  description               = "Quarkiverse team for the opencv extension"
+  create_default_maintainer = false
+  privacy                   = "closed"
+}
+
+# Add team to repository
+resource "github_team_repository" "quarkus_opencv" {
+  team_id    = github_team.quarkus_opencv.id
+  repository = github_repository.quarkus_opencv.name
+  permission = "maintain"
+}
+
+# Add users to the team
+resource "github_team_membership" "quarkus_opencv" {
+  for_each = { for tm in ["gsmet"] : tm => tm }
+  team_id  = github_team.quarkus_opencv.id
+  username = each.value
+  role     = "maintainer"
+}
