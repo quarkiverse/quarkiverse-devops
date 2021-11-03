d9aa378b709a5e967166486a34d34e69438277a2
George Gastaldi <gegastaldi@gmail.com>
2021-11-03 14:01:52 -0300

    Add quarkus-jgrapht.tf

---
+++ quarkus-jgrapht.tf
@@ -0,0 +1,32 @@
+# Create repository
+resource "github_repository" "quarkus_jgrapht" {
+  name                   = "quarkus-jgrapht"
+  description            = "Quarkus JGraphT extension"
+  delete_branch_on_merge = true
+  has_issues             = true
+  vulnerability_alerts   = true
+  topics                 = ["quarkus-extension"]
+}
+
+# Create team
+resource "github_team" "quarkus_jgrapht" {
+  name                      = "quarkiverse-jgrapht"
+  description               = "Quarkiverse team for the JGrapht extension"
+  create_default_maintainer = false
+  privacy                   = "closed"
+}
+
+# Add team to repository
+resource "github_team_repository" "quarkus_jgrapht" {
+  team_id    = github_team.quarkus_jgrapht.id
+  repository = github_repository.quarkus_jgrapht.name
+  permission = "maintain"
+}
+
+# Add users to the team
+resource "github_team_membership" "quarkus_jgrapht" {
+  for_each = { for tm in ["rsvoboda"] : tm => tm }
+  team_id  = github_team.quarkus_jgrapht.id
+  username = each.value
+  role     = "maintainer"
+}
