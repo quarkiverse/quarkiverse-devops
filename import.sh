61179ba2e8d6afa4c6f853f3ff3d1bc1a7bafba1
George Gastaldi <gegastaldi@gmail.com>
2021-11-03 14:43:17 -0300

    Include import script

---
+++ import.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+ : ${1:?"Must specify the team ID"}
+ : ${2:?"Must specify the repository to import "}
+ : ${3:?"Must specify the collaborator login"}
+
+TEAM=$1
+REPO=$2
+REPO_UNDER=$3
+COLLAB=$4
+
+terraform import github_repository.${REPO_UNDER} ${REPO} 
+terraform import github_team.${REPO_UNDER} ${TEAM} 
+terraform import github_team_repository.${REPO_UNDER} ${TEAM}:${REPO}
+terraform import "github_team_membership.${REPO_UNDER}[\"${COLLAB}\"]" ${TEAM}:${COLLAB} 
+
+terraform plan
