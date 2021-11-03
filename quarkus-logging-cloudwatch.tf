e6175f60497e9a0b6ec673d7921f410ebe77f554
George Gastaldi <gegastaldi@gmail.com>
2021-11-03 12:28:06 -0300

    Add quarkus-logging-cloudwatch.tf

---
+++ quarkus-logging-cloudwatch.tf
@@ -0,0 +1,32 @@
+# Create repository
+resource "github_repository" "quarkus_logging_cloudwatch" {
+  name                   = "quarkus-logging-cloudwatch"
+  description            = "Quarkus Amazon CloudWatch extension"
+  delete_branch_on_merge = true
+  has_issues             = true
+  vulnerability_alerts   = true
+  topics                 = ["cloudwatch", "quarkus-extension"]
+}
+
+# Create team
+resource "github_team" "quarkus_logging_cloudwatch" {
+  name                      = "quarkiverse-logging-cloudwatch"
+  description               = "Quarkiverse team for the logging-cloudwatch extension"
+  create_default_maintainer = false
+  privacy                   = "closed"
+}
+
+# Add team to repository
+resource "github_team_repository" "quarkus_logging_cloudwatch" {
+  team_id    = github_team.quarkus_logging_cloudwatch.id
+  repository = github_repository.quarkus_logging_cloudwatch.name
+  permission = "maintain"
+}
+
+# Add users to the team
+resource "github_team_membership" "quarkus_logging_cloudwatch" {
+  for_each = { for tm in ["pilhuhn", "bennetelli"] : tm => tm }
+  team_id  = github_team.quarkus_logging_cloudwatch.id
+  username = each.value
+  role     = "maintainer"
+}
