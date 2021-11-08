2405bca3f704eea41eed3fa183191f1ad18c2cd9
George Gastaldi <gegastaldi@gmail.com>
2021-11-07 22:16:11 -0300

    Add parent team to quarkiverse-tika

--- quarkus-tika.tf
+++ quarkus-tika.tf
@@ -14,6 +14,7 @@ resource "github_team" "quarkus_tika" {
   description               = "Quarkiverse team for the Tika extension"
   create_default_maintainer = false
   privacy                   = "closed"
+  parent_team_id            = data.github_team.quarkiverse_members.id
 }
 
 # Add team to repository
