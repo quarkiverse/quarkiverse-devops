a50742f15b8c6f2c77e714df043c701f5e8b4b53
Guillaume Smet <guillaume.smet@gmail.com>
2021-11-26 15:11:01 +0100

    Add quarkus-jgit
    Make main branch the default for JGit

---
+++ quarkus-jgit.tf
@@ -0,0 +1,45 @@
+# Create repository
+resource "github_repository" "quarkus_jgit" {
+  name                   = "quarkus-jgit"
+  description            = "Quarkus JGit extension"
+  delete_branch_on_merge = true
+  has_issues             = true
+  vulnerability_alerts   = true
+  topics                 = ["quarkus-extension"]
+}
+
+# Create team
+resource "github_team" "quarkus_jgit" {
+  name                      = "quarkiverse-jgit"
+  description               = "Quarkiverse team for the JGit extension"
+  create_default_maintainer = false
+  privacy                   = "closed"
+  parent_team_id            = data.github_team.quarkiverse_members.id
+}
+
+# Add team to repository
+resource "github_team_repository" "quarkus_jgit" {
+  team_id    = github_team.quarkus_jgit.id
+  repository = github_repository.quarkus_jgit.name
+  permission = "maintain"
+}
+
+# Add users to the team
+resource "github_team_membership" "quarkus_jgit" {
+  for_each = { for tm in ["gastaldi"] : tm => tm }
+  team_id  = github_team.quarkus_jgit.id
+  username = each.value
+  role     = "maintainer"
+}
+
+# Create main branch
+resource "github_branch" "quarkus_jgit" {
+  repository = github_repository.quarkus_jgit.name
+  branch     = "main"
+}
+
+# Set main branch as default
+resource "github_branch_default" "quarkus_jgit" {
+  repository = github_repository.quarkus_jgit.name
+  branch     = github_branch.quarkus_jgit.branch
+}
