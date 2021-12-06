006d39bad1179a82349767e6fbf784677327097d
Guillaume Smet <guillaume.smet@gmail.com>
2021-12-06 17:58:41 +0100

    Add Hibernate Search Extras repository

---
+++ quarkus-hibernate-search-extras.tf
@@ -0,0 +1,33 @@
+# Create repository
+resource "github_repository" "quarkus_hibernate_search_extras" {
+  name                   = "quarkus-hibernate-search-extras"
+  description            = "Quarkus Hibernate Search Extras extensions"
+  delete_branch_on_merge = true
+  has_issues             = true
+  vulnerability_alerts   = true
+  topics                 = ["quarkus-extension"]
+}
+
+# Create team
+resource "github_team" "quarkus_hibernate_search_extras" {
+  name                      = "quarkiverse-hibernate-search-extras"
+  description               = "Quarkiverse team for the Hibernate Search Extras extensions"
+  create_default_maintainer = false
+  privacy                   = "closed"
+  parent_team_id            = data.github_team.quarkiverse_members.id
+}
+
+# Add team to repository
+resource "github_team_repository" "quarkus_hibernate_search_extras" {
+  team_id    = github_team.quarkus_hibernate_search_extras.id
+  repository = github_repository.quarkus_hibernate_search_extras.name
+  permission = "maintain"
+}
+
+# Add users to the team
+resource "github_team_membership" "quarkus_hibernate_search_extras" {
+  for_each = { for tm in ["yrodiere"] : tm => tm }
+  team_id  = github_team.quarkus_hibernate_search_extras.id
+  username = each.value
+  role     = "maintainer"
+}
