bfa4aae51364d2f9458b276a11d08c7a19728357
George Gastaldi <gegastaldi@gmail.com>
2021-12-10 13:32:56 -0300

    Create quarkiverse/quarkus-renarde repository (#16)

---
+++ quarkus-renarde.tf
@@ -0,0 +1,33 @@
+# Create repository
+resource "github_repository" "quarkus_renarde" {
+  name                   = "quarkus-renarde"
+  description            = "Server-side Web Framework with Qute templating, magic/easier controllers, auth, reverse-routing"
+  delete_branch_on_merge = true
+  has_issues             = true
+  vulnerability_alerts   = true
+  topics                 = ["quarkus-extension"]
+}
+
+# Create team
+resource "github_team" "quarkus_renarde" {
+  name                      = "quarkiverse-renarde"
+  description               = "Quarkiverse renarde team"
+  create_default_maintainer = false
+  privacy                   = "closed"
+  parent_team_id            = data.github_team.quarkiverse_members.id
+}
+
+# Add team to repository
+resource "github_team_repository" "quarkus_renarde" {
+  team_id    = github_team.quarkus_renarde.id
+  repository = github_repository.quarkus_renarde.name
+  permission = "maintain"
+}
+
+# Add users to the team
+resource "github_team_membership" "quarkus_renarde" {
+  for_each = { for tm in ["FroMage"] : tm => tm }
+  team_id  = github_team.quarkus_renarde.id
+  username = each.value
+  role     = "maintainer"
+}
