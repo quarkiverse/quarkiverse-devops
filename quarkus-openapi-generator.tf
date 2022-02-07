c97d9f9b502d31226e7ae6b62d9da1dce9d23636
George Gastaldi <gegastaldi@gmail.com>
2022-02-07 12:07:32 -0300

    Introduce quarkus-openapi-generator
    Closes https://github.com/quarkusio/quarkus/issues/16401

---
+++ quarkus-openapi-generator.tf
@@ -0,0 +1,33 @@
+# Create repository
+resource "github_repository" "quarkus_openapi_generator" {
+  name                   = "quarkus-openapi-generator"
+  description            = "OpenApi Generator - REST Client Generator"
+  delete_branch_on_merge = true
+  has_issues             = true
+  vulnerability_alerts   = true
+  topics                 = ["quarkus-extension"]
+}
+
+# Create team
+resource "github_team" "quarkus_openapi_generator" {
+  name                      = "quarkiverse-openapi-generator"
+  description               = "openapi-generator team"
+  create_default_maintainer = false
+  privacy                   = "closed"
+  parent_team_id            = data.github_team.quarkiverse_members.id
+}
+
+# Add team to repository
+resource "github_team_repository" "quarkus_openapi_generator" {
+  team_id    = github_team.quarkus_openapi_generator.id
+  repository = github_repository.quarkus_openapi_generator.name
+  permission = "maintain"
+}
+
+# Add users to the team
+resource "github_team_membership" "quarkus_openapi_generator" {
+  for_each = { for tm in ["ricardozanini"] : tm => tm }
+  team_id  = github_team.quarkus_openapi_generator.id
+  username = each.value
+  role     = "maintainer"
+}
