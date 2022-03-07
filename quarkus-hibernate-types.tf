346a19c5898b2cbc1938b72df52c3b3c50c741e5
George Gastaldi <gegastaldi@gmail.com>
2022-03-07 11:04:05 -0300

    Fixed hibernate types team name

--- quarkus-hibernate-types.tf
+++ quarkus-hibernate-types.tf
@@ -22,7 +22,7 @@ resource "github_repository" "quarkus_hibernate_types" {
 
 # Create team
 resource "github_team" "quarkus_hibernate_types" {
-  name                      = "quarkiverse-hibernate_types"
+  name                      = "quarkiverse-hibernate-types"
   description               = "Quarkiverse team for the hibernate_types extension"
   create_default_maintainer = false
   privacy                   = "closed"
