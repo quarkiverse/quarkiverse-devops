29d1db1e9f3025ca581e325c40e784c2b0542aae
George Gastaldi <gegastaldi@gmail.com>
2022-05-24 13:10:56 -0300

    Introduce quarkus-apache-pinot

---
+++ quarkus-apache-pinot.tf
@@ -0,0 +1,40 @@
+# Create repository
+resource "github_repository" "quarkus_apache_pinot" {
+  name                   = "quarkus-apache-pinot"
+  description            = "Quarkus Apache Pinot extension"
+  archive_on_destroy     = true
+  delete_branch_on_merge = true
+  has_issues             = true
+  vulnerability_alerts   = true
+  topics                 = ["quarkus-extension"]
+  lifecycle {
+    ignore_changes = [
+      # Workaround for integrations/terraform-provider-github#1037.
+      branches,
+    ]
+  }
+}
+
+# Create team
+resource "github_team" "quarkus_apache_pinot" {
+  name                      = "quarkiverse-apache-pinot"
+  description               = "Quarkiverse team for the Apache Pinot extension"
+  create_default_maintainer = false
+  privacy                   = "closed"
+  parent_team_id            = data.github_team.quarkiverse_members.id
+}
+
+# Add team to repository
+resource "github_team_repository" "quarkus_apache_pinot" {
+  team_id    = github_team.quarkus_apache_pinot.id
+  repository = github_repository.quarkus_apache_pinot.name
+  permission = "maintain"
+}
+
+# Add users to the team
+resource "github_team_membership" "quarkus_apache_pinot" {
+  for_each = { for tm in ["loicmathieu"] : tm => tm }
+  team_id  = github_team.quarkus_apache_pinot.id
+  username = each.value
+  role     = "maintainer"
+}
