47dfcd94b748ff3030cfdb1d354c72abc1923885
George Gastaldi <gegastaldi@gmail.com>
2022-08-17 10:44:56 -0300

    Fix typo

--- quarkus-jdbc-sqlite.tf
+++ quarkus-jdbc-sqlite.tf
@@ -1,7 +1,7 @@
 # Create repository
 resource "github_repository" "quarkus_jdbc_sqlite" {
   name                   = "quarkus-jdbc-sqlite"
-  description            = "Basic SQLite driver for Quarkus with Agoral and native mode support"
+  description            = "Basic SQLite driver for Quarkus with Agroal and native mode support"
   archive_on_destroy     = true
   delete_branch_on_merge = true
   has_issues             = true
