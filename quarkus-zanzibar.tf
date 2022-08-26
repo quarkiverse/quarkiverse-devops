893a5a0c4beb09fba88a3f84c3bb5b624b98c664
George Gastaldi <gegastaldi@gmail.com>
2022-08-26 11:21:24 -0300

    Introduce quarkus-zanzibar
    - As requested in https://github.com/quarkusio/quarkus/issues/27331

---
+++ quarkus-zanzibar.tf
@@ -0,0 +1,42 @@
+# Create repository
+resource "github_repository" "quarkus_zanzibar" {
+  name                   = "quarkus-zanzibar"
+  description            = "Zanzibar style fine grained authorization"
+  homepage_url           = "https://zanzibar.academy/"
+  archive_on_destroy     = true
+  delete_branch_on_merge = true
+  has_issues             = true
+  vulnerability_alerts   = true
+
+  topics = ["quarkus-extension", "zanzibar"]
+  lifecycle {
+    ignore_changes = [
+      # Workaround for integrations/terraform-provider-github#1037.
+      branches,
+    ]
+  }
+}
+
+# Create team
+resource "github_team" "quarkus_zanzibar" {
+  name                      = "quarkiverse-zanzibar"
+  description               = "Quarkiverse team for the zanzibar Quarkus extension"
+  create_default_maintainer = false
+  privacy                   = "closed"
+  parent_team_id            = data.github_team.quarkiverse_members.id
+}
+
+# Add team to repository
+resource "github_team_repository" "quarkus_zanzibar" {
+  team_id    = github_team.quarkus_zanzibar.id
+  repository = github_repository.quarkus_zanzibar.name
+  permission = "maintain"
+}
+
+# Add users to the team
+resource "github_team_membership" "quarkus_zanzibar" {
+  for_each = { for tm in ["kdubb"] : tm => tm }
+  team_id  = github_team.quarkus_zanzibar.id
+  username = each.value
+  role     = "maintainer"
+}
