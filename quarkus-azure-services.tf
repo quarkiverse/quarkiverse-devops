3a417b2f8c98c72637a6ea3b237a804867b26e5b
George Gastaldi <gegastaldi@gmail.com>
2022-09-14 10:38:55 -0300

    Introduce quarkus-azure-services
    - Fixes https://github.com/quarkusio/quarkus/issues/27932

---
+++ quarkus-azure-services.tf
@@ -0,0 +1,40 @@
+# Create repository
+resource "github_repository" "quarkus_azure_services" {
+  name                   = "quarkus-azure-services"
+  description            = "Quarkus extensions for Azure services"
+  archive_on_destroy     = true
+  delete_branch_on_merge = true
+  has_issues             = true
+  vulnerability_alerts   = true
+  topics                 = ["quarkus-extension", "quarkus", "azure"]
+  lifecycle {
+    ignore_changes = [
+      # Workaround for integrations/terraform-provider-github#1037.
+      branches,
+    ]
+  }
+}
+
+# Create team
+resource "github_team" "quarkus_azure_services" {
+  name                      = "quarkiverse-azure-services"
+  description               = "Quarkiverse team for the Azure services extensions"
+  create_default_maintainer = false
+  privacy                   = "closed"
+  parent_team_id            = data.github_team.quarkiverse_members.id
+}
+
+# Add team to repository
+resource "github_team_repository" "quarkus_azure_services" {
+  team_id    = github_team.quarkus_azure_services.id
+  repository = github_repository.quarkus_azure_services.name
+  permission = "maintain"
+}
+
+# Add users to the team
+resource "github_team_membership" "quarkus_azure_services" {
+  for_each = { for tm in ["m-reza-rahman", "edburns", "majguo", "zhengchang907", "galiacheng"] : tm => tm }
+  team_id  = github_team.quarkus_azure_services.id
+  username = each.value
+  role     = "maintainer"
+}
