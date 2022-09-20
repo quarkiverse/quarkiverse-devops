5193d0f29c84a8079c14f370e20eda1348917e29
George Gastaldi <gegastaldi@gmail.com>
2022-09-20 15:35:15 -0300

    Introduce quarkus-authzed-client.tf
    - Fixes https://github.com/quarkusio/quarkus/issues/28058

---
+++ quarkus-authzed-client.tf
@@ -0,0 +1,41 @@
+# Create repository
+resource "github_repository" "quarkus_authzed_client" {
+  name                   = "quarkus-authzed-client"
+  description            = "An extension for connecting to authzed instances from Quarkus applications"
+  homepage_url           = "https://docs.authzed.com/reference/api"
+  archive_on_destroy     = true
+  delete_branch_on_merge = true
+  has_issues             = true
+  vulnerability_alerts   = true
+  topics                 = ["quarkus-extension", "authzed", "fga"]
+  lifecycle {
+    ignore_changes = [
+      # Workaround for integrations/terraform-provider-github#1037.
+      branches,
+    ]
+  }
+}
+
+# Create team
+resource "github_team" "quarkus_authzed_client" {
+  name                      = "quarkiverse-authzed-client"
+  description               = "authzed-client team"
+  create_default_maintainer = false
+  privacy                   = "closed"
+  parent_team_id            = data.github_team.quarkiverse_members.id
+}
+
+# Add team to repository
+resource "github_team_repository" "quarkus_authzed_client" {
+  team_id    = github_team.quarkus_authzed_client.id
+  repository = github_repository.quarkus_authzed_client.name
+  permission = "maintain"
+}
+
+# Add users to the team
+resource "github_team_membership" "quarkus_authzed_client" {
+  for_each = { for tm in ["iocanel"] : tm => tm }
+  team_id  = github_team.quarkus_authzed_client.id
+  username = each.value
+  role     = "maintainer"
+}
