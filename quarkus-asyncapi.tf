7e24a1808e2acc3e1b4880495fe5c61c45830df1
George Gastaldi <gegastaldi@gmail.com>
2022-11-17 10:06:46 -0300

    Introduce quarkus-asyncapi
    - Closes https://github.com/quarkusio/quarkus/issues/29311

---
+++ quarkus-asyncapi.tf
@@ -0,0 +1,36 @@
+# Create repository
+resource "github_repository" "quarkus_asyncapi" {
+  name                   = "quarkus-asyncapi"
+  description            = "AsyncAPI Quarkus configuration and metadata generator"
+  homepage_url           = "https://www.asyncapi.com/"
+  allow_update_branch    = true
+  archive_on_destroy     = true
+  delete_branch_on_merge = true
+  has_issues             = true
+  vulnerability_alerts   = true
+  topics                 = ["quarkus-extension", "asyncapi"]
+}
+
+# Create team
+resource "github_team" "quarkus_asyncapi" {
+  name                      = "quarkiverse-asyncapi"
+  description               = "AsyncAPI team"
+  create_default_maintainer = false
+  privacy                   = "closed"
+  parent_team_id            = data.github_team.quarkiverse_members.id
+}
+
+# Add team to repository
+resource "github_team_repository" "quarkus_asyncapi" {
+  team_id    = github_team.quarkus_asyncapi.id
+  repository = github_repository.quarkus_asyncapi.name
+  permission = "maintain"
+}
+
+# Add users to the team
+resource "github_team_membership" "quarkus_asyncapi" {
+  for_each = { for tm in ["fjtirado", "ricardozanini"] : tm => tm }
+  team_id  = github_team.quarkus_asyncapi.id
+  username = each.value
+  role     = "maintainer"
+}
