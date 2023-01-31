d306740fbbab71a25e0b95bedb298ec28fb831d8
George Gastaldi <gegastaldi@gmail.com>
2023-01-31 10:48:05 -0300

    Add @carlesarnal to quarkus-openapi-generator
    As discussed in https://github.com/quarkusio/quarkus/issues/18683

--- quarkus-openapi-generator.tf
+++ quarkus-openapi-generator.tf
@@ -28,7 +28,7 @@ resource "github_team_repository" "quarkus_openapi_generator" {
 
 # Add users to the team
 resource "github_team_membership" "quarkus_openapi_generator" {
-  for_each = { for tm in ["ricardozanini", "evacchi", "fjtirado", "hbelmiro"] : tm => tm }
+  for_each = { for tm in ["ricardozanini", "evacchi", "fjtirado", "hbelmiro", "carlesarnal"] : tm => tm }
   team_id  = github_team.quarkus_openapi_generator.id
   username = each.value
   role     = "maintainer"
