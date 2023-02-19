775982354110354f5b1f7c9fda608b8147bb85d8
George Gastaldi <gegastaldi@gmail.com>
2023-02-19 16:56:11 -0300

    Introduce quarkus-jasperreports
    - Fixes https://github.com/quarkusio/quarkus/issues/31288

---
+++ quarkus-jasperreports.tf
@@ -0,0 +1,36 @@
+# Create repository
+resource "github_repository" "quarkus_jasperreports" {
+  name                   = "quarkus-jasperreports"
+  description            = "Print reports created using JasperReports using the Java API"
+  homepage_url           = "http://jasperreports.sourceforge.net/"
+  allow_update_branch    = true
+  archive_on_destroy     = true
+  delete_branch_on_merge = true
+  has_issues             = true
+  vulnerability_alerts   = true
+  topics                 = ["quarkus-extension", "jasperreports"]
+}
+
+# Create team
+resource "github_team" "quarkus_jasperreports" {
+  name                      = "quarkiverse-jasperreports"
+  description               = "jasperreports team"
+  create_default_maintainer = false
+  privacy                   = "closed"
+  parent_team_id            = data.github_team.quarkiverse_members.id
+}
+
+# Add team to repository
+resource "github_team_repository" "quarkus_jasperreports" {
+  team_id    = github_team.quarkus_jasperreports.id
+  repository = github_repository.quarkus_jasperreports.name
+  permission = "maintain"
+}
+
+# Add users to the team
+resource "github_team_membership" "quarkus_jasperreports" {
+  for_each = { for tm in ["Postremus"] : tm => tm }
+  team_id  = github_team.quarkus_jasperreports.id
+  username = each.value
+  role     = "maintainer"
+}
