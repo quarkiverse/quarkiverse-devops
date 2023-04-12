b71a73b74ed7e5d2912fdd0fb88eac40949065c3
George Gastaldi <gegastaldi@gmail.com>
2023-04-12 14:21:27 -0300

    Introduce quarkus-bucket4j
    - Fixes https://github.com/quarkusio/quarkus/issues/32582

---
+++ quarkus-bucket4j.tf
@@ -0,0 +1,36 @@
+# Create repository
+resource "github_repository" "quarkus_bucket4j" {
+  name                   = "quarkus-bucket4j"
+  description            = "Java rate limiting library based on token-bucket algorithm Quarkus extension"
+  homepage_url           = "https://bucket4j.com/"
+  allow_update_branch    = true
+  archive_on_destroy     = true
+  delete_branch_on_merge = true
+  has_issues             = true
+  vulnerability_alerts   = true
+  topics                 = ["quarkus-extension"]
+}
+
+# Create team
+resource "github_team" "quarkus_bucket4j" {
+  name                      = "quarkiverse-bucket4j"
+  description               = "bucket4j team"
+  create_default_maintainer = false
+  privacy                   = "closed"
+  parent_team_id            = data.github_team.quarkiverse_members.id
+}
+
+# Add team to repository
+resource "github_team_repository" "quarkus_bucket4j" {
+  team_id    = github_team.quarkus_bucket4j.id
+  repository = github_repository.quarkus_bucket4j.name
+  permission = "maintain"
+}
+
+# Add users to the team
+resource "github_team_membership" "quarkus_bucket4j" {
+  for_each = { for tm in ["rmanibus"] : tm => tm }
+  team_id  = github_team.quarkus_bucket4j.id
+  username = each.value
+  role     = "maintainer"
+}
