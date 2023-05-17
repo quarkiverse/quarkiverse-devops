f763c8d77571ea025db1204f151917f7465e68cc
Andy Damevin <ia3andy@gmail.com>
2023-05-17 09:49:25 +0200

    Add edewit as maintainer on the web-bundler extension (#145)
    * Add edewith as maintainer on the web-bundler extension
    
    * Update quarkus-web-bundler.tf
    
    * Update quarkus-web-bundler.tf

--- quarkus-web-bundler.tf
+++ quarkus-web-bundler.tf
@@ -1,12 +1,12 @@
 # Create repository
 resource "github_repository" "quarkus_web_assets" {
   name                   = "quarkus-web-bundler"
-  description            = "Provide processing and helpers for your web-app media (JS, CSS, SCSS)."
+  description            = "Create full-stack web apps quickly and easily with this Quarkus extension. It offers zero-configuration bundling for your web app scripts (JS, JSX, TS, TSX), dependencies (jQuery, React, htmx, etc.), and styles (CSS, SCSS, SASS)."
   archive_on_destroy     = true
   delete_branch_on_merge = true
   has_issues             = true
   vulnerability_alerts   = true
-  topics                 = ["quarkus-extension", "frontend", "renarde"]
+  topics                 = ["quarkus-extension", "frontend", "web", "full-stack", "js", "scss", "sass", "bundle", "esbuild", "mvnpm", "webjars", "typescript", "javascript", "assets"]
 }
 
 # Create team
@@ -27,7 +27,7 @@ resource "github_team_repository" "quarkus_web_assets" {
 
 # Add users to the team
 resource "github_team_membership" "quarkus_web_assets" {
-  for_each = { for tm in ["ia3andy", "mkouba", "phillip-kruger", "FroMage"] : tm => tm }
+  for_each = { for tm in ["ia3andy", "mkouba", "phillip-kruger", "FroMage", "edewit"] : tm => tm }
   team_id  = github_team.quarkus_web_assets.id
   username = each.value
   role     = "maintainer"
