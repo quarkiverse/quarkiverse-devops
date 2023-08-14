94aa39f377a60bb564ef75079c2715d522e2939f
George Gastaldi <gegastaldi@gmail.com>
2023-08-14 15:22:21 -0300

    Create quarkus-ironjacamar.tf

---
+++ terraform-scripts/quarkus-ironjacamar.tf
@@ -0,0 +1,36 @@
+# Create repository
+resource "github_repository" "quarkus_ironjacamar" {
+  name                   = "quarkus-ironjacamar"
+  description            = "IronJacamar is an implementation of the Jakarta Connector Architecture specification"
+  homepage_url           = "https://some-url/"
+  allow_update_branch    = true
+  archive_on_destroy     = true
+  delete_branch_on_merge = true
+  has_issues             = true
+  vulnerability_alerts   = true
+  topics                 = ["quarkus-extension", "jca"]
+}
+
+# Create team
+resource "github_team" "quarkus_ironjacamar" {
+  name                      = "quarkiverse-ironjacamar"
+  description               = "ironjacamar team"
+  create_default_maintainer = false
+  privacy                   = "closed"
+  parent_team_id            = data.github_team.quarkiverse_members.id
+}
+
+# Add team to repository
+resource "github_team_repository" "quarkus_ironjacamar" {
+  team_id    = github_team.quarkus_ironjacamar.id
+  repository = github_repository.quarkus_ironjacamar.name
+  permission = "maintain"
+}
+
+# Add users to the team
+resource "github_team_membership" "quarkus_ironjacamar" {
+  for_each = { for tm in ["gastaldi"] : tm => tm }
+  team_id  = github_team.quarkus_ironjacamar.id
+  username = each.value
+  role     = "maintainer"
+}
