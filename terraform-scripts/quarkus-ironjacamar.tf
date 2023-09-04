2872cbe14c3e9cce741078aa042e6052b844a833
George Gastaldi <gegastaldi@gmail.com>
2023-09-03 23:15:57 -0300

    Add @vsevel to quarkus-ironjacamar
    So you can review PRs

--- terraform-scripts/quarkus-ironjacamar.tf
+++ terraform-scripts/quarkus-ironjacamar.tf
@@ -29,7 +29,7 @@ resource "github_team_repository" "quarkus_ironjacamar" {
 
 # Add users to the team
 resource "github_team_membership" "quarkus_ironjacamar" {
-  for_each = { for tm in ["gastaldi"] : tm => tm }
+  for_each = { for tm in ["gastaldi", "vsevel"] : tm => tm }
   team_id  = github_team.quarkus_ironjacamar.id
   username = each.value
   role     = "maintainer"
