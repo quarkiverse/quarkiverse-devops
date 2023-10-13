fb89b721766242d93f1633a15e86d66c835687be
George Gastaldi <gegastaldi@gmail.com>
2023-10-13 15:18:59 -0300

    Introduce quarkus-mailpit (#185)
    - Fixes https://github.com/quarkusio/quarkus/issues/32894

---
+++ terraform-scripts/quarkus-mailpit.tf
@@ -0,0 +1,36 @@
+# Create repository
+resource "github_repository" "quarkus_mailpit" {
+  name                   = "quarkus-mailpit"
+  description            = "An email and SMTP testing tool with API for developers"
+  homepage_url           = "https://github.com/axllent/mailpit"
+  allow_update_branch    = true
+  archive_on_destroy     = true
+  delete_branch_on_merge = true
+  has_issues             = true
+  vulnerability_alerts   = true
+  topics                 = ["quarkus-extension"]
+}
+
+# Create team
+resource "github_team" "quarkus_mailpit" {
+  name                      = "quarkiverse-mailpit"
+  description               = "mailpit team"
+  create_default_maintainer = false
+  privacy                   = "closed"
+  parent_team_id            = data.github_team.quarkiverse_members.id
+}
+
+# Add team to repository
+resource "github_team_repository" "quarkus_mailpit" {
+  team_id    = github_team.quarkus_mailpit.id
+  repository = github_repository.quarkus_mailpit.name
+  permission = "maintain"
+}
+
+# Add users to the team
+resource "github_team_membership" "quarkus_mailpit" {
+  for_each = { for tm in ["melloware"] : tm => tm }
+  team_id  = github_team.quarkus_mailpit.id
+  username = each.value
+  role     = "maintainer"
+}
