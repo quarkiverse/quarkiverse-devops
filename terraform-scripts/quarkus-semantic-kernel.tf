ecde9a0a0e1f93309ad8031a3abd41bec28e1f96
George Gastaldi <gegastaldi@gmail.com>
2023-10-24 11:15:47 -0300

    Introduce `quarkus-semantic-kernel` (#188)

---
+++ terraform-scripts/quarkus-semantic-kernel.tf
@@ -0,0 +1,36 @@
+# Create repository
+resource "github_repository" "quarkus_semantic_kernel" {
+  name                   = "quarkus-semantic-kernel"
+  description            = "This extension eases the integration of Semantic Kernel for Java with Quarkus"
+  homepage_url           = "https://learn.microsoft.com/en-us/semantic-kernel/overview/"
+  allow_update_branch    = true
+  archive_on_destroy     = true
+  delete_branch_on_merge = true
+  has_issues             = true
+  vulnerability_alerts   = true
+  topics                 = ["quarkus-extension", "ai", "artificial-intelligence", "openai", "azure-openai", "hugging-face"]
+}
+
+# Create team
+resource "github_team" "quarkus_semantic_kernel" {
+  name                      = "quarkiverse-semantic-kernel"
+  description               = "semantic-kernel team"
+  create_default_maintainer = false
+  privacy                   = "closed"
+  parent_team_id            = data.github_team.quarkiverse_members.id
+}
+
+# Add team to repository
+resource "github_team_repository" "quarkus_semantic_kernel" {
+  team_id    = github_team.quarkus_semantic_kernel.id
+  repository = github_repository.quarkus_semantic_kernel.name
+  permission = "maintain"
+}
+
+# Add users to the team
+resource "github_team_membership" "quarkus_semantic_kernel" {
+  for_each = { for tm in ["agoncal"] : tm => tm }
+  team_id  = github_team.quarkus_semantic_kernel.id
+  username = each.value
+  role     = "maintainer"
+}
