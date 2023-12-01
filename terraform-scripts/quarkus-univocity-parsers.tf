ecef30eaf82faacf3768e85f326d917538f72117
George Gastaldi <gegastaldi@gmail.com>
2023-12-01 09:00:15 -0300

    Remove LGTM from quarkus-univocity-parsers (#194)

--- terraform-scripts/quarkus-univocity-parsers.tf
+++ terraform-scripts/quarkus-univocity-parsers.tf
@@ -33,11 +33,3 @@ resource "github_team_membership" "quarkus_univocity_parsers" {
   username = each.value
   role     = "maintainer"
 }
-
-# Enable apps in repository
-resource "github_app_installation_repository" "quarkus_univocity_parsers" {
-  for_each = { for app in [local.applications.lgtm] : app => app }
-  # The installation id of the app (in the organization).
-  installation_id = each.value
-  repository      = github_repository.quarkus_univocity_parsers.name
-}
