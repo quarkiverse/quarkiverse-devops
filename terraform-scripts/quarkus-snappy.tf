762209b2abfe28ae3a6b86500fbc59980f3f46a9
George Gastaldi <gegastaldi@gmail.com>
2023-12-05 12:30:44 -0300

    Introduce `quarkus-snappy` (#195)
    - Closes https://github.com/quarkusio/quarkus/issues/37510

---
+++ terraform-scripts/quarkus-snappy.tf
@@ -0,0 +1,36 @@
+# Create repository
+resource "github_repository" "quarkus_snappy" {
+  name                   = "quarkus-snappy"
+  description            = "Snappy compressor in native environment"
+  homepage_url           = "https://github.com/google/snappy"
+  allow_update_branch    = true
+  archive_on_destroy     = true
+  delete_branch_on_merge = true
+  has_issues             = true
+  vulnerability_alerts   = true
+  topics                 = ["quarkus-extension", "snappy-native", "compression"]
+}
+
+# Create team
+resource "github_team" "quarkus_snappy" {
+  name                      = "quarkiverse-snappy"
+  description               = "snappy team"
+  create_default_maintainer = false
+  privacy                   = "closed"
+  parent_team_id            = data.github_team.quarkiverse_members.id
+}
+
+# Add team to repository
+resource "github_team_repository" "quarkus_snappy" {
+  team_id    = github_team.quarkus_snappy.id
+  repository = github_repository.quarkus_snappy.name
+  permission = "maintain"
+}
+
+# Add users to the team
+resource "github_team_membership" "quarkus_snappy" {
+  for_each = { for tm in ["MarcAnB"] : tm => tm }
+  team_id  = github_team.quarkus_snappy.id
+  username = each.value
+  role     = "maintainer"
+}
