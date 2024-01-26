726dba57d1157c3c0576ac9632fd529454edfd43
George Gastaldi <gegastaldi@gmail.com>
2024-01-26 18:23:45 -0300

    Sync quarkus-kiota.tf

--- terraform-scripts/quarkus-kiota.tf
+++ terraform-scripts/quarkus-kiota.tf
@@ -4,6 +4,8 @@ resource "github_repository" "quarkus_kiota" {
   description            = "Generate client SDKs using Kiota from OpenAPI descriptions"
   homepage_url           = "https://github.com/microsoft/kiota"
   allow_update_branch    = true
+  allow_merge_commit     = true
+  allow_rebase_merge     = true
   archive_on_destroy     = true
   delete_branch_on_merge = true
   has_issues             = true
