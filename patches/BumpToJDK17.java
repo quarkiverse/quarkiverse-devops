2fe13a8dfd62977b937f0286a8a83a67973bce7c
George Gastaldi <gegastaldi@gmail.com>
2024-02-06 16:25:02 -0300

    Bump build and release script to JDK 17 (#193)

--- patches/BumpToJDK17.java
+++ patches/BumpToJDK17.java
@@ -9,10 +9,29 @@ import java.nio.file.Path;
 public class BumpToJDK17 {
 
     public static void main(String... args) throws Exception {
-        Path workflowFile = Path.of(".github/workflows/quarkus-snapshot.yaml");
-        if (Files.exists(workflowFile)) {
-            Files.writeString(workflowFile,
-                    Files.readString(workflowFile).replace("JAVA_VERSION: 11", "JAVA_VERSION: 17"));
+        // Update snapshot workflow
+        {
+            Path workflowFile = Path.of(".github/workflows/quarkus-snapshot.yaml");
+            if (Files.exists(workflowFile)) {
+                Files.writeString(workflowFile,
+                        Files.readString(workflowFile).replace("JAVA_VERSION: 11", "JAVA_VERSION: 17"));
+            }
+        }
+        // Update release workflow
+        {
+            Path workflowFile = Path.of(".github/workflows/release.yml");
+            if (Files.exists(workflowFile)) {
+                String contents = Files.readString(workflowFile);
+                Files.writeString(workflowFile, contents.replace("JDK 11", "JDK 17").replace("java-version: 11", "java-version: 17"));
+            }
+        }
+        // Update build.yaml as well
+        {
+            Path workflowFile = Path.of(".github/workflows/build.yml");
+            if (Files.exists(workflowFile)) {
+                String contents = Files.readString(workflowFile);
+                Files.writeString(workflowFile, contents.replace("JDK 11", "JDK 17").replace("java-version: 11", "java-version: 17"));
+            }
         }
     }
 }
