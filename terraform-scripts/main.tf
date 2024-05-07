bb4945de447b4d8de2fdde7843237556ffca0c46
George Gastaldi <gegastaldi@gmail.com>
2024-05-07 11:39:59 -0300

    Upgrade Terraform

--- terraform-scripts/main.tf
+++ terraform-scripts/main.tf
@@ -2,7 +2,7 @@ terraform {
   required_providers {
     github = {
       source  = "integrations/github"
-      version = "~> 5.0"
+      version = "~> 6.0"
     }
   }
 
@@ -14,7 +14,7 @@ terraform {
     }
   }
 
-  required_version = "~> 1.7.0"
+  required_version = "~> 1.8.0"
 }
 
 provider "github" {}
