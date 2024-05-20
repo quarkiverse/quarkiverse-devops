afa2eae6ae59ad0e0d9899246d16b771b7d24b5b
George Gastaldi <gegastaldi@gmail.com>
2024-05-20 10:54:11 -0300

    Introduce `quarkus-nagios` (#235)

---
+++ terraform-scripts/quarkus-nagios.tf
@@ -0,0 +1,36 @@
+# Create repository
+resource "github_repository" "quarkus_nagios" {
+  name                   = "quarkus-nagios"
+  description            = "Health endpoints in Nagios format"
+  homepage_url           = "https://docs.quarkiverse.io/quarkus-nagios/dev/"
+  allow_update_branch    = true
+  archive_on_destroy     = true
+  delete_branch_on_merge = true
+  has_issues             = true
+  vulnerability_alerts   = true
+  topics                 = ["quarkus-extension", "nagios", "observability"]
+}
+
+# Create team
+resource "github_team" "quarkus_nagios" {
+  name                      = "quarkiverse-nagios"
+  description               = "nagios team"
+  create_default_maintainer = false
+  privacy                   = "closed"
+  parent_team_id            = data.github_team.quarkiverse_members.id
+}
+
+# Add team to repository
+resource "github_team_repository" "quarkus_nagios" {
+  team_id    = github_team.quarkus_nagios.id
+  repository = github_repository.quarkus_nagios.name
+  permission = "maintain"
+}
+
+# Add users to the team
+resource "github_team_membership" "quarkus_nagios" {
+  for_each = { for tm in ["derari"] : tm => tm }
+  team_id  = github_team.quarkus_nagios.id
+  username = each.value
+  role     = "maintainer"
+}
