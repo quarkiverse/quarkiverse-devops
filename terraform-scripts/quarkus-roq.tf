2b479fd724ccde43d811a3721a6607a6ec369478
Andy Damevin <ia3andy@gmail.com>
2024-06-25 15:29:20 +0200

    Update and rename quarkus-statiq.tf to quarkus-roq.tf (#242)
    * Update and rename quarkus-statiq.tf to quarkus-roq.tf
    
    * Update CODEOWNERS
    
    * Update quarkus-roq.tf
    
    ---------
    
    Co-authored-by: George Gastaldi <gegastaldi@gmail.com>

--- terraform-scripts/quarkus-roq.tf
+++ terraform-scripts/quarkus-roq.tf
@@ -1,6 +1,6 @@
 # Create repository
 resource "github_repository" "quarkus_statiq" {
-  name                   = "quarkus-statiq"
+  name                   = "quarkus-roq"
   description            = "An extension to generate/publish static pages from your Quarkus web-app"
   allow_update_branch    = true
   archive_on_destroy     = true
@@ -8,13 +8,13 @@ resource "github_repository" "quarkus_statiq" {
   has_issues             = true
   has_projects           = true
   vulnerability_alerts   = true
-  topics                 = ["quarkus-extension", "web", "static", "generate", "blog"]
+  topics                 = ["quarkus-extension", "web", "static", "ssg", "site", "generator", "generate", "blog"]
 }
 
 # Create team
 resource "github_team" "quarkus_statiq" {
-  name                      = "quarkiverse-statiq"
-  description               = "statiq team"
+  name                      = "quarkiverse-roq"
+  description               = "roq team"
   create_default_maintainer = false
   privacy                   = "closed"
   parent_team_id            = data.github_team.quarkiverse_members.id
@@ -29,7 +29,7 @@ resource "github_team_repository" "quarkus_statiq" {
 
 # Add users to the team
 resource "github_team_membership" "quarkus_statiq" {
-  for_each = { for tm in ["ia3andy"] : tm => tm }
+  for_each = { for tm in ["ia3andy", "melloware", "mcruzdev"] : tm => tm }
   team_id  = github_team.quarkus_statiq.id
   username = each.value
   role     = "maintainer"
