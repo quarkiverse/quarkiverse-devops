290f65fc30ea3261f0b04824e4392610a835afcd
George Gastaldi <gegastaldi@gmail.com>
2024-07-22 22:15:28 -0300

    Update quarkus-kerberos.tf

--- terraform-scripts/quarkus-kerberos.tf
+++ terraform-scripts/quarkus-kerberos.tf
@@ -2,6 +2,7 @@
 resource "github_repository" "quarkus_kerberos" {
   name                   = "quarkus-kerberos"
   description            = "Quarkus Kerberos extension"
+  allow_update_branch    = true
   archive_on_destroy     = true
   delete_branch_on_merge = true
   has_issues             = true
