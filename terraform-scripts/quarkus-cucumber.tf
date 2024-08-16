bf9e16bd9228374e9339682e45426405d75a54b9
George Gastaldi <gegastaldi@gmail.com>
2024-08-16 08:14:03 -0300

    Update quarkus-cucumber.tf

--- terraform-scripts/quarkus-cucumber.tf
+++ terraform-scripts/quarkus-cucumber.tf
@@ -2,6 +2,7 @@
 resource "github_repository" "quarkus_cucumber" {
   name                   = "quarkus-cucumber"
   description            = "Quarkus Cucumber extension"
+  allow_update_branch    = true
   archive_on_destroy     = true
   delete_branch_on_merge = true
   has_issues             = true
