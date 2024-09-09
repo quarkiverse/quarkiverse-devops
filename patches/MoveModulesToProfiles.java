3348f205fbc8f9551b329214f3f188ad75f4acb9
George Gastaldi <gegastaldi@gmail.com>
2024-09-09 10:33:02 -0300

    Also move integration-tests if not already done

--- patches/MoveModulesToProfiles.java
+++ patches/MoveModulesToProfiles.java
@@ -1,5 +1,4 @@
 ///usr/bin/env jbang "$0" "$@" ; exit $?
-//DEPS org.l2x6.pom-tuner:pom-tuner:4.3.0
 //DEPS io.fabric8:maven-model-helper:37
 
 import io.fabric8.maven.Maven;
@@ -13,12 +12,13 @@ import java.nio.file.Path;
 import java.util.List;
 
 /**
- * Move the <module>docs</module> to a Maven profile
+ * Move the <module>docs</module> and <module>integration-tests</module> to Maven profiles
  */
-public class MoveDocsModule {
+public class MoveModulesToProfiles {
 
     public static void main(String... args) {
         Path parent = Path.of("pom.xml");
+        boolean writeModel = false;
         if (Files.exists(parent)) {
             Model model = Maven.readModel(parent);
             List<String> modules = model.getModules();
@@ -36,8 +36,25 @@ public class MoveDocsModule {
                 docsProfile.addModule("docs");
                 // Add docs as the first profile
                 profiles.add(0, docsProfile);
-                Maven.writeModel(model);
+                writeModel = true;
             }
+            if (modules.remove("integration-tests")) {
+                List<Profile> profiles = model.getProfiles();
+                Profile itProfile = new Profile();
+                itProfile.setId("it");
+                Activation activation = new Activation();
+                ActivationProperty activationProperty = new ActivationProperty();
+                activationProperty.setName("performRelease");
+                activationProperty.setValue("!true");
+                activation.setProperty(activationProperty);
+                itProfile.setActivation(activation);
+                itProfile.addModule("integration-tests");
+                // Add docs as the first profile
+                profiles.add(itProfile);
+                writeModel = true;
+            }
+            if (writeModel)
+                Maven.writeModel(model);
         }
     }
 }
