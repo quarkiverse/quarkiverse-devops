b0caa100b8feeeeab9e76de9e0933d6ea87fedd5
Melloware <mellowaredev@gmail.com>
2024-09-11 15:06:22 -0400

    Update terraform-scripts/quarkus-mdns.tf
    Co-authored-by: George Gastaldi <gegastaldi@gmail.com>

--- terraform-scripts/quarkus-mdns.tf
+++ terraform-scripts/quarkus-mdns.tf
@@ -2,7 +2,7 @@
 resource "github_repository" "quarkus_mdns" {
   name                   = "quarkus-mdns"
   description            = "Quarkus mDNS extension - Multicast Domain Name System"
-  homepage_url           = "https://docs.quarkiverse.io/quarkus-jmdns/dev/"
+  homepage_url           = "https://docs.quarkiverse.io/quarkus-mdns/dev/"
   allow_update_branch    = true
   archive_on_destroy     = true
   delete_branch_on_merge = true
