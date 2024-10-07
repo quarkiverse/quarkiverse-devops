9b02bff0d1ff7a3ccde0ffa9c90665f71c64d7be
George Gastaldi <gegastaldi@gmail.com>
2024-10-07 08:50:24 -0300

    Create quarkus-infinispan-embedded.tf

---
+++ terraform-scripts/quarkus-infinispan-embedded.tf
@@ -0,0 +1,36 @@
+# Create repository
+resource "github_repository" "quarkus_infinispan_embedded" {
+  name                   = "quarkus-infinispan-embedded"
+  description            = "A Quarkus extension for embedded Infinispan integrates in-memory data grid features directly into Quarkus applications, enabling efficient caching and data storage."
+  homepage_url           = "https://docs.quarkiverse.io/quarkus-infinispan-embedded/dev"
+  allow_update_branch    = true
+  archive_on_destroy     = true
+  delete_branch_on_merge = true
+  has_issues             = true
+  vulnerability_alerts   = true
+  topics                 = ["quarkus-extension", "infinispan", "infinispan-embedded", "caching", "distributed-caching"]
+}
+
+# Create team
+resource "github_team" "quarkus_infinispan_embedded" {
+  name                      = "quarkiverse-infinispan-embedded"
+  description               = "infinispan-embedded team"
+  create_default_maintainer = false
+  privacy                   = "closed"
+  parent_team_id            = data.github_team.quarkiverse_members.id
+}
+
+# Add team to repository
+resource "github_team_repository" "quarkus_infinispan_embedded" {
+  team_id    = github_team.quarkus_infinispan_embedded.id
+  repository = github_repository.quarkus_infinispan_embedded.name
+  permission = "maintain"
+}
+
+# Add users to the team
+resource "github_team_membership" "quarkus_infinispan_embedded" {
+  for_each = { for tm in ["karesti", "wburns", "ryanemerson", "tristantarrant"] : tm => tm }
+  team_id  = github_team.quarkus_infinispan_embedded.id
+  username = each.value
+  role     = "maintainer"
+}
