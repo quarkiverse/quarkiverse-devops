f6e08e6d1f659b3b1fd88104db2619ab5fc9986b
George Gastaldi <gegastaldi@gmail.com>
2024-10-11 21:07:10 -0300

    Update JGit description + topics

--- terraform-scripts/quarkus-jgit.tf
+++ terraform-scripts/quarkus-jgit.tf
@@ -1,13 +1,13 @@
 # Create repository
 resource "github_repository" "quarkus_jgit" {
   name                   = "quarkus-jgit"
-  description            = "Quarkus JGit extension"
+  description            = "JGit is a lightweight, pure Java library implementing the Git version control system"
   allow_update_branch    = true
   archive_on_destroy     = true
   delete_branch_on_merge = true
   has_issues             = true
   vulnerability_alerts   = true
-  topics                 = ["quarkus-extension"]
+  topics                 = ["quarkus-extension", "jgit", "git", "gitea"]
 }
 
 # Create team
