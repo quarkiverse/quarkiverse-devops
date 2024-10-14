f5986a9840693efb7ae30408e4ebe0f0f0c84e22
George Gastaldi <gegastaldi@gmail.com>
2024-10-14 10:38:23 -0300

    Removing outs

--- patches/UpdateReleaseWorkflow.java
+++ patches/UpdateReleaseWorkflow.java
@@ -12,9 +12,7 @@ public class UpdateReleaseWorkflow {
         Path preRelease = Path.of(".github/workflows/pre-release.yml");
         Path releasePrepare = Path.of(".github/workflows/release-perform.yml");
         Path releasePerform = Path.of(".github/workflows/release-prepare.yml");
-        System.out.println("-> RELEASE WORKFLOW EXISTS? " + Files.exists(releaseWorkflow));
         if (Files.deleteIfExists(releaseWorkflow)) {
-            System.out.println("Copying");
             Files.copy(Path.of("/Users/ggastald/workspace/quarkiverse/quarkus-jgit/.github/workflows/pre-release.yml"), preRelease, StandardCopyOption.REPLACE_EXISTING);
             Files.copy(Path.of("/Users/ggastald/workspace/quarkiverse/quarkus-jgit/.github/workflows/release-prepare.yml"), releasePrepare);
             Files.copy(Path.of("/Users/ggastald/workspace/quarkiverse/quarkus-jgit/.github/workflows/release-perform.yml"), releasePerform);
