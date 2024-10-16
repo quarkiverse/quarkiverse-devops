cc21f8a0882b8285fcf021655101a5598b29d68a
George Gastaldi <gegastaldi@gmail.com>
2024-10-16 10:40:19 -0300

    Protect Bucket4j main branch
    - Block Force pushes
    - Require a PR to merge to main

--- terraform-scripts/quarkus-bucket4j.tf
+++ terraform-scripts/quarkus-bucket4j.tf
@@ -3,6 +3,8 @@ resource "github_repository" "quarkus_bucket4j" {
   name                   = "quarkus-bucket4j"
   description            = "Java rate limiting library based on token-bucket algorithm Quarkus extension"
   homepage_url           = "https://bucket4j.com/"
+  allow_merge_commit     = false
+  allow_rebase_merge     = false
   allow_update_branch    = true
   archive_on_destroy     = true
   delete_branch_on_merge = true
@@ -35,3 +37,34 @@ resource "github_team_membership" "quarkus_bucket4j" {
   username = each.value
   role     = "maintainer"
 }
+
+# Protect main branch using a ruleset
+resource "github_repository_ruleset" "quarkus_bucket4j" {
+  name        = "main"
+  repository  = github_repository.quarkus_bucket4j.name
+  target      = "branch"
+  enforcement = "active"
+
+  conditions {
+    ref_name {
+      include = ["~DEFAULT_BRANCH"]
+      exclude = []
+    }
+  }
+
+  bypass_actors {
+    actor_id    = data.github_app.quarkiverse_ci.id
+    actor_type  = "Integration"
+    bypass_mode = "always"
+  }
+
+  rules {
+    # Prevent force push
+    non_fast_forward = true
+    # Require pull request reviews before merging
+    pull_request {
+
+    }
+  }
+}
+
