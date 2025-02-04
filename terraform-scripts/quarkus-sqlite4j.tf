521c5268db0c09446b4eaa9ff625e84c353a6dd7
George Gastaldi <gegastaldi@gmail.com>
2025-02-04 18:03:58 -0300

    Introduce `quarkus-sqlite4j`
    ```
    - Introduce Terraform script to automate creation and access configuration for the Quarkus SQLite4j GitHub repository.
    - Configure code ownership for the Quarkus SQLite4j Terraform module to ensure maintainability and review standards.
    ```

---
+++ terraform-scripts/quarkus-sqlite4j.tf
@@ -0,0 +1,36 @@
+# Create repository
+resource "github_repository" "quarkus_sqlite4j" {
+  name                   = "quarkus-sqlite4j"
+  description            = "SQLite driver for Quarkus with Agroal in pure Java built on SQLite4j"
+  homepage_url           = "https://docs.quarkiverse.io/quarkus-sqlite4j/dev"
+  allow_update_branch    = true
+  archive_on_destroy     = true
+  delete_branch_on_merge = true
+  has_issues             = true
+  vulnerability_alerts   = true
+  topics                 = ["quarkus-extension"]
+}
+
+# Create team
+resource "github_team" "quarkus_sqlite4j" {
+  name                      = "quarkiverse-sqlite4j"
+  description               = "sqlite4j team"
+  create_default_maintainer = false
+  privacy                   = "closed"
+  parent_team_id            = data.github_team.quarkiverse_members.id
+}
+
+# Add team to repository
+resource "github_team_repository" "quarkus_sqlite4j" {
+  team_id    = github_team.quarkus_sqlite4j.id
+  repository = github_repository.quarkus_sqlite4j.name
+  permission = "maintain"
+}
+
+# Add users to the team
+resource "github_team_membership" "quarkus_sqlite4j" {
+  for_each = { for tm in ["andreaTP"] : tm => tm }
+  team_id  = github_team.quarkus_sqlite4j.id
+  username = each.value
+  role     = "maintainer"
+}
