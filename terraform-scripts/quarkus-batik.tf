d6bf2a6280557f5e7986a49572c56ebe701f87a3
Melloware <mellowaredev@gmail.com>
2025-08-03 14:38:11 -0400

    Create quarkus-batik.tf (#362)
    * Create quarkus-batik.tf
    
    * Update CODEOWNERS
    
    * Update CODEOWNERS
    
    * Update CODEOWNERS

---
+++ terraform-scripts/quarkus-batik.tf
@@ -0,0 +1,36 @@
+# Create repository
+resource "github_repository" "quarkus_batik" {
+  name                   = "quarkus-batik"
+  description            = "Batik supports Scalable Vector Graphics (SVG) format for various purposes, such as display, generation or manipulation."
+  homepage_url           = "https://xmlgraphics.apache.org/batik/"
+  allow_update_branch    = true
+  archive_on_destroy     = true
+  delete_branch_on_merge = true
+  has_issues             = true
+  vulnerability_alerts   = true
+  topics                 = ["quarkus-extension", "svg", "graphics", "image"]
+}
+
+# Create team
+resource "github_team" "quarkus_batik" {
+  name                      = "quarkiverse-batik"
+  description               = "batik team"
+  create_default_maintainer = false
+  privacy                   = "closed"
+  parent_team_id            = data.github_team.quarkiverse_members.id
+}
+
+# Add team to repository
+resource "github_team_repository" "quarkus_batik" {
+  team_id    = github_team.quarkus_batik.id
+  repository = github_repository.quarkus_batik.name
+  permission = "maintain"
+}
+
+# Add users to the team
+resource "github_team_membership" "quarkus_batik" {
+  for_each = { for tm in ["melloware"] : tm => tm }
+  team_id  = github_team.quarkus_batik.id
+  username = each.value
+  role     = "maintainer"
+}
