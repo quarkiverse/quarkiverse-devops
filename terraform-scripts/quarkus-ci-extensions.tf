3a695887341000597901b4c83e4776593f1d1556
George Gastaldi <gegastaldi@gmail.com>
2025-08-05 11:03:11 -0300

    Introduce quarkus-ci-extensions (#364)

---
+++ terraform-scripts/quarkus-ci-extensions.tf
@@ -0,0 +1,34 @@
+# Create repository
+resource "github_repository" "quarkus_ci_extensions" {
+  name                   = "quarkus-ci-extensions"
+  description            = "Quarkus extensions for generating CI related pipelines / workflows"
+  archive_on_destroy     = true
+  delete_branch_on_merge = true
+  has_issues             = true
+  vulnerability_alerts   = true
+  topics                 = ["quarkus-extension"]
+}
+
+# Create team
+resource "github_team" "quarkus_ci_extensions" {
+  name                      = "quarkiverse-ci-extensions"
+  description               = "Quarkiverse team for the ci-extensions extension"
+  create_default_maintainer = false
+  privacy                   = "closed"
+  parent_team_id            = data.github_team.quarkiverse_members.id
+}
+
+# Add team to repository
+resource "github_team_repository" "quarkus_ci_extensions" {
+  team_id    = github_team.quarkus_ci_extensions.id
+  repository = github_repository.quarkus_ci_extensions.name
+  permission = "maintain"
+}
+
+# Add users to the team
+resource "github_team_membership" "quarkus_ci_extensions" {
+  for_each = { for tm in ["iocanel"] : tm => tm }
+  team_id  = github_team.quarkus_ci_extensions.id
+  username = each.value
+  role     = "maintainer"
+}
