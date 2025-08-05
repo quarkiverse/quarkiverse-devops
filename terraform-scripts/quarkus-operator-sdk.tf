4ae5ce8b9ef9db2dbf508bcf24d2ad59d990ea56
George Gastaldi <gegastaldi@gmail.com>
2025-08-05 10:38:38 -0300

    Sync with quarkus-operator-sdk repo settings (#363)
    * Sync with quarkus-operator-sdk repo settings
    
    * Update quarkus-operator-sdk.tf

--- terraform-scripts/quarkus-operator-sdk.tf
+++ terraform-scripts/quarkus-operator-sdk.tf
@@ -3,6 +3,7 @@ resource "github_repository" "quarkus_operator_sdk" {
   name                   = "quarkus-operator-sdk"
   description            = "Quarkus Extension to create Kubernetes Operators in Java using the Java Operator SDK (https://github.com/java-operator-sdk/java-operator-sdk) project"
   allow_update_branch    = true
+  allow_merge_commit     = false
   archive_on_destroy     = true
   delete_branch_on_merge = true
   has_issues             = true
