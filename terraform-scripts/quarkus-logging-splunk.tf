820bbce1ecb4a49855e1d4973ee6ca2956d16bc2
George Gastaldi <gegastaldi@gmail.com>
2025-11-11 12:22:45 -0300

    Sync changes

--- terraform-scripts/quarkus-logging-splunk.tf
+++ terraform-scripts/quarkus-logging-splunk.tf
@@ -5,6 +5,7 @@ resource "github_repository" "quarkus_logging_splunk" {
   archive_on_destroy     = true
   delete_branch_on_merge = true
   has_issues             = true
+  has_wiki               = true
   vulnerability_alerts   = true
   topics                 = ["quarkus-extension"]
 
