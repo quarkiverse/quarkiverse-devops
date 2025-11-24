2bdb967a29115b9adfe91a1f77ad503702fd6291
George Gastaldi <gegastaldi@gmail.com>
2025-11-24 09:44:10 -0300

    Set up repository and permissions for quarkus-mapstruct
    - Provision infrastructure for the `quarkus-mapstruct` repository, including repository settings, team configuration, and branch protection rules.
    - Establish automated code ownership by integrating `@quarkiverse/quarkiverse-mapstruct` into the `.github/CODEOWNERS` file.

---
+++ terraform-scripts/quarkus-mapstruct.tf
@@ -0,0 +1,66 @@
+# Create repository
+resource "github_repository" "quarkus_mapstruct" {
+  name                   = "quarkus-mapstruct"
+  description            = "MapStruct is a code generator that greatly simplifies the implementation of mappings between Java bean types based on a convention over configuration approach."
+  homepage_url           = "https://docs.quarkiverse.io/quarkus-mapstruct/dev"
+  allow_update_branch    = true
+  archive_on_destroy     = true
+  delete_branch_on_merge = true
+  has_issues             = true
+  vulnerability_alerts   = true
+  topics                 = ["quarkus-extension", "mapstruct"]
+}
+
+# Create team
+resource "github_team" "quarkus_mapstruct" {
+  name                      = "quarkiverse-mapstruct"
+  description               = "mapstruct team"
+  create_default_maintainer = false
+  privacy                   = "closed"
+  parent_team_id            = data.github_team.quarkiverse_members.id
+}
+
+# Add team to repository
+resource "github_team_repository" "quarkus_mapstruct" {
+  team_id    = github_team.quarkus_mapstruct.id
+  repository = github_repository.quarkus_mapstruct.name
+  permission = "maintain"
+}
+
+# Add users to the team
+resource "github_team_membership" "quarkus_mapstruct" {
+  for_each = { for tm in ["Postremus"] : tm => tm }
+  team_id  = github_team.quarkus_mapstruct.id
+  username = each.value
+  role     = "maintainer"
+}
+
+# Protect main branch using a ruleset
+resource "github_repository_ruleset" "quarkus_mapstruct" {
+  name        = "main"
+  repository  = github_repository.quarkus_mapstruct.name
+  target      = "branch"
+  enforcement = "active"
+
+  conditions {
+    ref_name {
+      include = ["~DEFAULT_BRANCH"]
+      exclude = []
+    }
+  }
+
+  bypass_actors {
+    actor_id    = data.github_app.quarkiverse_ci.id
+    actor_type  = "Integration"
+    bypass_mode = "always"
+  }
+
+  rules {
+    # Prevent force push
+    non_fast_forward = true
+    # Require pull request reviews before merging
+    pull_request {
+
+    }
+  }
+}
