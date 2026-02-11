6307dea236887a359f3f20eef5ba02083cf00b92
Ricardo Zanini <1538000+ricardozanini@users.noreply.github.com>
2026-02-11 11:40:56 -0500

    Add new user 'dgutierr' to Quarkus Flow

--- terraform-scripts/quarkus-flow.tf
+++ terraform-scripts/quarkus-flow.tf
@@ -55,7 +55,7 @@ resource "github_team_repository" "quarkus_flow_triage" {
 
 # Add users to the team
 resource "github_team_membership" "quarkus_flow_triage" {
-  for_each = { for tm in ["wmedvede", "gmunozfe", "domhanak", "mcruzdev", "fantonangeli", "lornakelly", "kumaradityaraj"] : tm => tm }
+  for_each = { for tm in ["wmedvede", "gmunozfe", "domhanak", "mcruzdev", "fantonangeli", "lornakelly", "kumaradityaraj", "dgutierr"] : tm => tm }
   team_id  = github_team.quarkus_flow_triage.id
   username = each.value
   role     = "member"
