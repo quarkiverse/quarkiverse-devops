9d22af4d39797c5faa589d8e94eb6f149b902de3
George Gastaldi <gegastaldi@gmail.com>
2026-05-05 10:00:51 -0300

    Rename 'quarkus-resteasy-problem' to 'quarkus-http-problem'

--- terraform-scripts/quarkus-http-problem.tf
+++ terraform-scripts/quarkus-http-problem.tf
@@ -1,8 +1,8 @@
 # Create repository
 resource "github_repository" "quarkus_resteasy_problem" {
-  name                   = "quarkus-resteasy-problem"
+  name                   = "quarkus-http-problem"
   description            = "Unified error responses for Quarkus REST APIs via Problem Details for HTTP APIs (RFC9457 & RFC7807)"
-  homepage_url           = "https://docs.quarkiverse.io/quarkus-resteasy-problem/dev"
+  homepage_url           = "https://docs.quarkiverse.io/quarkus-http-problem/dev"
   allow_update_branch    = true
   allow_merge_commit     = true
   allow_rebase_merge     = true
@@ -21,8 +21,8 @@ resource "github_repository_vulnerability_alerts" "quarkus_resteasy_problem" {
 
 # Create team
 resource "github_team" "quarkus_resteasy_problem" {
-  name           = "quarkiverse-resteasy-problem"
-  description    = "resteasy-problem team"
+  name           = "quarkiverse-http-problem"
+  description    = "http-problem team"
   privacy        = "closed"
   parent_team_id = data.github_team.quarkiverse_members.id
 }
