eff4b66c528c1062f16d3ab9787ef91dbf55d059
George Gastaldi <gegastaldi@gmail.com>
2026-05-21 10:47:36 -0300

    Add topic to quarkus-prettytime

--- terraform-scripts/quarkus-prettytime.tf
+++ terraform-scripts/quarkus-prettytime.tf
@@ -7,7 +7,7 @@ resource "github_repository" "quarkus_prettytime" {
   archive_on_destroy     = true
   delete_branch_on_merge = true
   has_issues             = true
-  topics                 = ["quarkus-extension", "prettytime"]
+  topics                 = ["quarkus-extension", "prettytime", "ocpsoft"]
 
   # Do not use the template below in new repositories. This is kept for backward compatibility with existing repositories
   template {
