0d6ebb38b73cdaa9c74076f7bcebf448670d279d
George Gastaldi <gegastaldi@gmail.com>
2026-06-08 12:24:22 -0300

    Add GitHub repository provisioning for quarkus-clock (#456)
    Add GitHub repository provisioning for quarkus-clock (#54692)

---
+++ terraform-scripts/quarkus-clock.tf
@@ -0,0 +1,70 @@
+# Create repository
+resource "github_repository" "quarkus_clock" {
+  name                   = "quarkus-clock"
+  description            = "Injectable, testable, and high-precision java.time.Clock implementations for Quarkus"
+  homepage_url           = "https://docs.quarkiverse.io/quarkus-clock/dev"
+  allow_update_branch    = true
+  archive_on_destroy     = true
+  delete_branch_on_merge = true
+  has_issues             = true
+  topics                 = ["quarkus-extension", "clock", "time", "ntp", "ptp", "testing", "java-time"]
+}
+
+# Enable vulnerability alerts
+resource "github_repository_vulnerability_alerts" "quarkus_clock" {
+  repository = github_repository.quarkus_clock.name
+  enabled    = true
+}
+
+# Create team
+resource "github_team" "quarkus_clock" {
+  name           = "quarkiverse-clock"
+  description    = "clock team"
+  privacy        = "closed"
+  parent_team_id = data.github_team.quarkiverse_members.id
+}
+
+# Add team to repository
+resource "github_team_repository" "quarkus_clock" {
+  team_id    = github_team.quarkus_clock.id
+  repository = github_repository.quarkus_clock.name
+  permission = "push"
+}
+
+# Add users to the team
+resource "github_team_membership" "quarkus_clock" {
+  for_each = { for tm in ["asakaxgit"] : tm => tm }
+  team_id  = github_team.quarkus_clock.id
+  username = each.value
+  role     = "maintainer"
+}
+
+# Protect main branch using a ruleset
+resource "github_repository_ruleset" "quarkus_clock" {
+  name        = "main"
+  repository  = github_repository.quarkus_clock.name
+  target      = "branch"
+  enforcement = "active"
+
+  conditions {
+    ref_name {
+      include = ["~DEFAULT_BRANCH"]
+      exclude = []
+    }
+  }
+
+  bypass_actors {
+    actor_id    = data.github_app.quarkiverse_ci.id
+    actor_type  = "Integration"
+    bypass_mode = "always"
+  }
+
+  rules {
+    # Prevent force push
+    non_fast_forward = true
+    # Require pull request reviews before merging
+    pull_request {
+
+    }
+  }
+}
