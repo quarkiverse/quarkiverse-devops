49696bcef43f868824b0ad930e7fdbc6ee1b3f80
George Gastaldi <gegastaldi@gmail.com>
2026-06-28 11:26:38 -0300

    Rename from quarkus_chicory to quarkus_endive

--- terraform-scripts/quarkus-endive.tf
+++ terraform-scripts/quarkus-endive.tf
@@ -1,5 +1,5 @@
 # Create repository
-resource "github_repository" "quarkus_chicory" {
+resource "github_repository" "quarkus_endive" {
   name                   = "quarkus-endive"
   description            = "A Quarkus extension to easily integrate Endive(WASM runtime) in your applications"
   homepage_url           = "https://docs.quarkiverse.io/quarkus-endive/dev"
@@ -10,13 +10,13 @@ resource "github_repository" "quarkus_chicory" {
   topics                 = ["wasm", "endive", "sandbox", "quarkus-extension"]
 }
 
-resource "github_repository_vulnerability_alerts" "quarkus_chicory" {
-  repository = github_repository.quarkus_chicory.name
+resource "github_repository_vulnerability_alerts" "quarkus_endive" {
+  repository = github_repository.quarkus_endive.name
   enabled    = true
 }
 
 # Create team
-resource "github_team" "quarkus_chicory" {
+resource "github_team" "quarkus_endive" {
   name           = "quarkiverse-endive"
   description    = "endive team"
   privacy        = "closed"
@@ -24,16 +24,16 @@ resource "github_team" "quarkus_chicory" {
 }
 
 # Add team to repository
-resource "github_team_repository" "quarkus_chicory" {
-  team_id    = github_team.quarkus_chicory.id
-  repository = github_repository.quarkus_chicory.name
+resource "github_team_repository" "quarkus_endive" {
+  team_id    = github_team.quarkus_endive.id
+  repository = github_repository.quarkus_endive.name
   permission = "push"
 }
 
 # Add users to the team
-resource "github_team_membership" "quarkus_chicory" {
+resource "github_team_membership" "quarkus_endive" {
   for_each = { for tm in ["fabiobrz", "andreaTP"] : tm => tm }
-  team_id  = github_team.quarkus_chicory.id
+  team_id  = github_team.quarkus_endive.id
   username = each.value
   role     = "maintainer"
 }
