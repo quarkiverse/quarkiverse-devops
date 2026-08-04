86c99b67462bb7fa0f16c3038c7598069bac7813
George Gastaldi <gegastaldi@gmail.com>
2026-08-04 13:23:24 -0300

    Enable GitHub app installation for Quarkus JGit
    Add GitHub app installation resource for Quarkus JGit.

--- terraform-scripts/quarkus-jgit.tf
+++ terraform-scripts/quarkus-jgit.tf
@@ -69,3 +69,11 @@ resource "github_repository_ruleset" "quarkus_jgit" {
     }
   }
 }
+
+# Enable apps in repository
+resource "github_app_installation_repository" "quarkus_jgit" {
+  for_each = { for app in [local.applications.renovate] : app => app }
+  # The installation id of the app (in the organization).
+  installation_id = each.value
+  repository      = github_repository.quarkus_jgit.name
+}
