9acbb419abe1f9d33562681653f1bf71babd482f
Ed Burns <edburns@microsoft.com>
2026-09-11 09:37:49 -0700

    Change Azure client, tenant, and subscription IDs (#503)
    Hello 
    Ed, long time no see. Regarding to the transition of Azure Quarkus Extensions repo, I left a new comment  in your transition doc and assigned it to you. To make sure you are aware of it, also copied here:
    
    The CI of Azure Quarkus extensions repo uses my personal Azure account for creating Aure resesources during the integration test: quarkus-azure-services/.github/workflows/build.yml at main · quarkiverse/quarkus-azure-services . Pls
    
    Follow the inline instructions to set up a new FIC to enable OIDC authentication with Azure: quarkus-azure-services/.github/workflows/build.yml at main · quarkiverse/quarkus-azure-services .
    Send out a PR to update values for 3 environment variables AZURE_CLIENT_ID, AZURE_TENANT_ID and AZURE_SUBSCRIPTION_ID which are used in CI pipeline above: quarkiverse-devops/terraform-scripts/quarkus-azure-services.tf at main · quarkiverse/quarkiverse-de… 
    I will remove the FIC and uami from my personal Azure account by this week.

--- terraform-scripts/quarkus-azure-services.tf
+++ terraform-scripts/quarkus-azure-services.tf
@@ -40,9 +40,9 @@ resource "github_team_membership" "quarkus_azure_services" {
 # Create CI environment variables
 resource "github_actions_environment_variable" "quarkus_azure_services" {
   for_each = {
-    "AZURE_CLIENT_ID"       = "5afa9765-4baa-416d-a8dd-636f79aa1705"
-    "AZURE_TENANT_ID"       = "63b290a9-485b-4dc9-a980-777b22108974"
-    "AZURE_SUBSCRIPTION_ID" = "2be34c77-b1be-4417-b4b7-5419ba89dfed"
+    "AZURE_CLIENT_ID"       = "bd82fc0f-b99a-4a77-9d93-4d21b99e5533"
+    "AZURE_TENANT_ID"       = "72f988bf-86f1-41af-91ab-2d7cd011db47"
+    "AZURE_SUBSCRIPTION_ID" = "05887623-95c5-4e50-a71c-6e1c738794e2"
   }
   repository    = github_repository.quarkus_azure_services.name
   environment   = "ci"
